90017898494a128e3e35e4401a7c61c598f39cbb
Mike <michaelsteewart09@gmail.com>
2023-11-19 23:55:47 -0500

    service 2

--- app/main.py
+++ app/main.py
@@ -1,52 +1,51 @@
 import asyncio
 import logging
 from fastapi import HTTPException, status, FastAPI
-import requests
 import random
-from mangum import Mangum
-import uvicorn
-import requests
-from starlette.types import ASGIApp
-
-random.seed(54321)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
-# app: ASGIApp = FastAPI(on_startup=[_startup_event])
-handler = Mangum(app)
 
 @app.get("/")
 async def read_root():
-    logger.info(" INFO logging test ")
-    logger.warning(" WARNING test ")
-    logger.error(" ERROR log test")
     return {"Hello": "World"}
+
 
 @app.get("/ping")
 async def health_check():
-    logger.info("This is message /ping")
+
+    logger.info(" INFO logging test ")
+    logger.warning(" WARNING test ")
+    logger.error(" ERROR log test")
+
     return "ping"
 
-@app.get("/internal-api")
-def internal_api():
-    logger.info("This is message start /internal-pi ")
-    response = requests.get(f"https://google.com")
-    status_code_response = response.status_code
-    content_response = response.content
-    response.close()
-    logger.info("This is message close /internal-API ")
-    message = f" 'statis code:' + {status_code_response}  + content: + {content_response} "
-    return message
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    if item_id % 2 == 0:
+        # mock io - wait for x seconds
+        seconds = random.uniform(0, 3)
+        await asyncio.sleep(seconds)
+    return {"item_id": item_id, "q": q}
 
-@app.get("/external-api")
-def internal_api():
-    logger.info("This is message start /external-API")
-    response = requests.get(f"hhttps://google.com")
-    status_code_response = response.status_code
-    content_response = response.content
-    response.close()
-    logger.info("This is message close /internal-pi ")
-    message = f" 'statis code:' + {status_code_response}  + content: + {content_response} "
-    return message
+
+@app.get("/invalid")
+async def invalid():
+    logger.warning("This is message /invalid")
+    raise ValueError("Invalid ")
+
+@app.get("/exception")
+async def exception():
+    try:
+        raise ValueError("exception")
+    except Exception as ex:
+        # generate random number
+        seconds = random.uniform(0, 30)
+        # Create the exception. 
+        exception = IOError("Failed at " + str(seconds))
+        # Update status to failed.
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"HTTP 500 internal server error")
